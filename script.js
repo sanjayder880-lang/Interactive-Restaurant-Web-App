b065caa924b140564952845cd586a82e5714a51e
sanjayder880-lang <sanjayder880@gmail.com>
2026-01-30 14:43:34 +0530

    add update HTML CSS

---
+++ script.js
@@ -0,0 +1,7 @@
+const items = [
+{id:1, name:"Margherita Pizza", cat:"pizza", price:250, img:"https://images.unsplash.com/photo-1601924638867-3ec4cfa36c1a" },
+  { id:2, name:"Pepperoni Pizza", cat:"pizza", price:300, img:"https://images.unsplash.com/photo-1593560708920-61dd98c46a19" },
+  { id:3, name:"Veg Burger", cat:"burger", price:150, img:"https://images.unsplash.com/photo-1550547660-d9450f859349" },
+  { id:4, name:"Chicken Burger", cat:"burger", price:180, img:"https://images.unsplash.com/photo-1550317138-10000687a72b" },
+  { id:5, name:"Coke", cat:"drink", price:60, img:"https://images.unsplash.com/photo-1629203851122-3726ecdf080e" }
+];
